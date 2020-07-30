1857686f7f7bcbd9775330312329886a395a44e6
AndiAKbar09 <andidna1234@gmail.com>
2020-07-30 13:07:21 +0700

    first studying

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="d-flex">
+            <div class="card-0">
+                <div class="card-body">
+                    <div class="mr-5">
+                        <h2 class="font-weight-bolder">Welcome to SPBU 34-17104</h2>
+                    </div>
+                    <img src="{{asset('img/pas.jpg')}}" class="rounded mx-auto d-block" width="100%" height="140%" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
